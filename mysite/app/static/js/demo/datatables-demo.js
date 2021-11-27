5874b2a9f89deb9bb0c92d8732b28a579a5b78e3
Hwisun Bae <hwisunbae.hb@gmail.com>
2021-11-27 09:00:26 +0000

    reading tweets done

--- mysite/app/static/js/demo/datatables-demo.js
+++ mysite/app/static/js/demo/datatables-demo.js
@@ -1,14 +1,4 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable-positive').DataTable({
-    "scrollY": "200px",
-  });
 
-  $('#dataTable-negative').DataTable({
-    "scrollY": "200px",
-  });
-
-  $('#dataTable-neutral').DataTable({
-    "scrollY": "200px",
-  });
 });
